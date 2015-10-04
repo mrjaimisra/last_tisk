21caba7c71ea020620e40995d74e3a3d719a4839
mrjaimisra <misrajai01@gmail.com>
2015-10-03 18:12:05 -0600

    Add spec for user unarchives list

--- spec/features/user_archives_and_unarchives_task_list_spec.rb
+++ spec/features/user_archives_and_unarchives_task_list_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "User archives a task list", type: :feature do
+RSpec.feature "User changes the status of a task list", type: :feature do
 
   let!(:user) { User.create(username: "Jai", password: "123") }
   let!(:list) { List.create(title: "Things To Do", user_id: user.id)}
 
-  scenario "successfully" do
+  scenario "to archived" do
     visit lists_path
 
     expect(page).to have_link(list.title)
@@ -27,6 +27,31 @@ RSpec.feature "User archives a task list", type: :feature do
     click_link "Archived Lists"
 
     expect(current_path).to eq(archived_lists_path)
+    expect(page).to have_content(list.title)
+  end
+
+  scenario "to unarchived" do
+    visit lists_path
+
+    expect(page).to have_link(list.title)
+
+    click_on list.title
+
+    expect(current_path).to eq(list_path(list))
+
+    click_on "Archive"
+
+    expect(current_path).to eq(list_path(list))
+    expect(page).to have_content("archived")
+
+    click_on "Unarchive"
+
+    expect(current_path).to eq(list_path(list))
+    expect(page).to have_content("unarchived")
+
+    click_link "All Lists"
+
+    expect(current_path).to eq(lists_path)
     expect(page).to have_content(list.title)
   end
 end
