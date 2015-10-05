a1f805d0dd07aa6962a802a3170c03596df4d1d1
mrjaimisra <misrajai01@gmail.com>
2015-10-05 04:33:15 -0600

    Add bootstrap gem Add user logout functionality Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   namespace :users, path: ":user", as: :user do
     resources :lists, only: [:index, :show, :new, :create, :update] do
